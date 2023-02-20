21205794e69cd1db3c3b6652d9ec474171f86f90
Felipp Piran <felippaugusto15@gmail.com>
2023-02-20 16:07:55 -0300

    updating files 2023-02-20

--- JS/index.js
+++ JS/index.js
@@ -1,7 +1,10 @@
 // variables globals
+const body = document.querySelector("body");
 const header = document.querySelector(".header");
 const loginOrSignUpText = document.querySelector("#loginOrSignUp p")
 const themeWhiteDark = document.querySelector("#theme-white-or-dark");
+const hardwareShowcase = document.querySelector(".hardware-showcase");
+const columnProducts = document.querySelectorAll(".column-products");
 const socialMedias = document.querySelector(".social-medias");
 const footer = document.querySelector(".footer")
 const textLogoHeader = document.querySelector(".text-logo-header");
@@ -9,12 +12,20 @@ const containerIntel = document.querySelector(".containerIntel");
 const containerAmd = document.querySelector(".containerAmd");
 const selectIntel = document.querySelector(".btnSelectIntel");
 const selectAmd = document.querySelector(".btnSelectAmd");
+const selectAmdOrIntel = document.querySelector(".selectAmdOrIntel");
 
 // theme white or dark
 themeWhiteDark.addEventListener("click", function() {
+    body.classList.toggle("active");
     themeWhiteDark.classList.toggle("active");
     header.classList.toggle("active");
     textLogoHeader.classList.toggle("active");
+
+    columnProducts.forEach(itens => {
+        itens.classList.toggle("active");
+    })
+
+    hardwareShowcase.classList.toggle("active");
     footer.classList.toggle("active");
     socialMedias.classList.toggle("active");
     loginOrSignUpText.classList.toggle("active");
@@ -26,10 +37,18 @@ const toggleBtnIntelAmd = {
             containerIntel.addEventListener("mouseover", function() {
             selectIntel.classList.add("active");
         })
+
+        selectIntel.addEventListener("mouseover", function() {
+            selectIntel.classList.add("active");
+        })
     },
 
-    intelDesactive() {
+    intelDesactive() {;
             containerIntel.addEventListener("mouseout", function() {
+            selectIntel.classList.remove("active");
+        })
+
+        selectIntel.addEventListener("mouseout", function() {
             selectIntel.classList.remove("active");
         })
     },
@@ -38,10 +57,18 @@ const toggleBtnIntelAmd = {
             containerAmd.addEventListener("mouseover", function() {
             selectAmd.classList.add("active");
         })
+
+        selectAmd.addEventListener("mouseover", function() {
+            selectAmd.classList.add("active");
+        })
     },
 
     amdDesactive() {
             containerAmd.addEventListener("mouseout", function() {
+            selectAmd.classList.remove("active");
+        })
+
+        selectAmd.addEventListener("mouseout", function() {
             selectAmd.classList.remove("active");
         })
     }
