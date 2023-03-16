dc9c9309814813da82fbd73d06d74d23adf6a296
Felipp Piran <felippaugusto15@gmail.com>
2023-03-15 21:44:02 -0300

    files updated 2023-03-15

---
+++ JS/building-computer.js
@@ -0,0 +1,21 @@
+const exit = $(".exit");
+const boxModelChooseHardwares = $(".box-model-choose-hardwares");
+const btnChooseHardware = $(".btn-choose-hardware");
+
+exit.click(() => {
+    boxModelChooseHardwares.removeClass("active");
+})
+
+btnChooseHardware.click(() => {
+    document.documentElement.scrollTop = 0;
+    boxModelChooseHardwares.addClass("active");
+})
+
+window.addEventListener("scroll", () => {
+    if(document.documentElement.scrollTop > 200) {
+        goToHeader.addClass("active");
+    }
+    else {
+        goToHeader.removeClass("active");
+    }
+})
